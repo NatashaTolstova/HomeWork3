cb2ad58e798383d0856762b7b883a5ea9721c7b4
NATASHA <klimknatasha@yandex.ru>
2024-10-30 19:36:23 +0300

    4

--- 4.c
+++ 4.c
@@ -3,3 +3,39 @@
 Данные на входе: Целое число от 1 до 12 - номер месяца.
 Данные на выходе: Время года на английском: winter, spring, summer, autumn
 */
+
+#include <stdio.h>
+int main(void)
+{
+    int month;
+    scanf("%d", &month);
+
+    switch (month)
+    {
+    case 12:
+    case 1:
+    case 2:
+        printf("winter");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        printf("spring");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        printf("summer");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        printf("autumn");
+        break;
+    
+    default:
+        printf("invalid input data :(");
+    }
+
+    return 0;
+}
