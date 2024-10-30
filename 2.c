4d1e5c5598304bdb46d65b3a0a558f53339e3a21
NATASHA <klimknatasha@yandex.ru>
2024-10-30 19:17:25 +0300

    2

--- 2.c
+++ 2.c
@@ -3,4 +3,18 @@
 
 Данные на входе: Пять целых чисел разделенных пробелом
 Данные на выходе: Одно целое число
-*\
+*/
+
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+int a,b,c,d,e;
+ scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+int min = a < b ? a : b;
+ min = min < c ? min : c;
+ min = min < d ? min : d;
+ min = min < e ? min : e;
+ printf("%d",min);
+ return 0;
+}
+
