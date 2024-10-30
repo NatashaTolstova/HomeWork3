c466ab75cdbc1890f1c97bb4951d7cfda7315306
NATASHA <klimknatasha@yandex.ru>
2024-10-30 19:29:44 +0300

    3

--- 3.c
+++ 3.c
@@ -4,3 +4,12 @@
 Данные на входе: Три целых числа
 Данные на выходе: Одно слово YES или NO
 */
+
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+int a,b,c;
+ scanf("%d%d%d",&a,&b,&c);
+printf("%s", a < b & b < c ? "YES" : "NO");
+ return 0;
+}
