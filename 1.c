8603c92a5109089c025bbd08e1e879b5ca9fa086
NATASHA <klimknatasha@yandex.ru>
2024-10-30 19:11:45 +0300

    1

--- 1.c
+++ 1.c
@@ -4,3 +4,16 @@
 Данные на входе: Пять целых чисел разделенных пробелом
 Данные на выходе: Одно целое числоа %d+%d+%d=%d
 */
+
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+int a,b,c,d,e;
+ scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+int max = a > b ? a : b;
+ max = max > c ? max : c;
+ max = max > d ? max : d;
+ max = max > e ? max : e;
+ printf("%d",max);
+ return 0;
+}
